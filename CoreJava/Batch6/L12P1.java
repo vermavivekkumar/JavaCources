3fabbaa9f5d8dcc4d89576faf2315fcc7733ab2b
Arth <vermavivekkumar@users.noreply.github.com>
2019-09-10 09:23:14 +0530

    Lecture 12: Ternary and Boolean logical operator

---
+++ CoreJava/Batch6/L12P1.java
@@ -0,0 +1,15 @@
+class L12P1{ // Ternary Operator
+	public static void main(String[] ss){
+		
+		int mark;
+		
+		String result;
+		
+		mark = 26;
+		
+		result = mark > 39 ? "Pass" : "Fail"; // Ternary Operator
+		
+		System.out.println(result);
+		
+	}
+}
---
+++ CoreJava/Batch6/L12P2.java
@@ -0,0 +1,13 @@
+class L12P2{ // Boolean Logical Operator
+			// Do it yourself
+	public static void main(String[] ss){
+		
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		boolean b3 = b1 & b2; // AND
+		
+		System.out.println(b3);
+		
+	}
+}
