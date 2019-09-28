bc2655f7bd2c14cbd72b702b4b5933653938752f
Arth <vermavivekkumar@users.noreply.github.com>
2019-09-28 08:57:37 +0530

    Lecture 21: Array

---
+++ CoreJava/Batch6/L21P1.java
@@ -0,0 +1,11 @@
+class L21P1{
+	public static void main(String args[]){
+		
+		int[] a = {100,101,110};
+		
+		
+		for(int i=0; i<3; i++)
+		System.out.println(a[i]);
+	}
+	
+}
---
+++ CoreJava/Batch6/L21P2.java
@@ -0,0 +1,17 @@
+class L21P2{
+	// Problem: What is the output
+	public static void main(String args[]){
+		
+		int[][] a = {{100,101,110},{1,2,3}};
+		
+		
+		for(int i=0; i<2; i++){
+			System.out.println(a[i]);
+			
+			for(int j=0; j<3 ;j++)
+			System.out.println(a[i][j]);
+		
+		}
+	}
+	
+}
