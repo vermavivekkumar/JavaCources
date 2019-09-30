3e5796dd253d4ec1a239d374580f66f0b9ccf421
Arth <vermavivekkumar@users.noreply.github.com>
2019-09-30 09:39:45 +0530

    Lecture 22: Array

---
+++ CoreJava/Batch6/L22P1.java
@@ -0,0 +1,17 @@
+class L22P1{
+	public static void main(String args[]){
+		
+		
+		int[][] a = {{100,101},{1,2,3},{10,20,30,878,767}};
+		
+		
+		for(int i=0; i<a.length; i++){
+			System.out.println(a[i]);
+			
+			for(int j=0; j<a[i].length ;j++)
+			System.out.println(a[i][j]);
+		
+		}
+	}
+	
+}
